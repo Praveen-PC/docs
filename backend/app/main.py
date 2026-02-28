b2f478d198694aecdd93929848763ce8ef863f53
Praveen-Nasiwak <praveen.nasiwak@outlook.com>
2026-02-28 17:32:03 +0530

    utl

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://docs-ochre-nu.vercel.app/login","https://docs-ochre-nu.vercel.app"],        # allow frontend
+    allow_origins=["http://localhost:5173","https://docs-ochre-nu.vercel.app"],        # allow frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
